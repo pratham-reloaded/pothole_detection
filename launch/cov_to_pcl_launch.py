bcc70dced617365daff3a2bfa296a7bc122c3db9
yashas3013 <katteyashas@gmail.com>
2023-05-08 08:22:57 +0530

    clean

--- launch/cov_to_pcl_launch.py
+++ launch/cov_to_pcl_launch.py
@@ -42,6 +42,27 @@ def generate_launch_description():
                 ],
                 output='screen',
             ),
+            launch_ros.actions.ComposableNodeContainer(
+                name='lane',
+                namespace='',
+                package='rclcpp_components',
+                executable='component_container',
+                composable_node_descriptions=[
+                    # Driver itself
+                    launch_ros.descriptions.ComposableNode(
+                        package='depth_image_proc',
+                        plugin='depth_image_proc::PointCloudXyzNode',
+                        name='point_cloud_lane',
+                        remappings=[
+                            ('image_rect', '/lane_depth'),
+                            ('camera_info', '/cov_info'),
+                            ('image', '/camera/color/image_raw'),
+                            ('/points', '/lane_points'),
+                        ],
+                    ),
+                ],
+                output='screen',
+            ),
             # rviz
             # launch_ros.actions.Node(
             # package='rviz2', node_executable='rviz2', output='screen',
